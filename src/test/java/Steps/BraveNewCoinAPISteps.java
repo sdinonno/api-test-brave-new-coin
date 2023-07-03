c1b96188ffa2481da0d8a5f424032ddbffe809ec
Sofia Di Nonno <sofidinonno@gmail.com>
2023-07-02 22:02:18 -0300

    Update steps for scenarios

--- src/test/java/Steps/BraveNewCoinAPISteps.java
+++ src/test/java/Steps/BraveNewCoinAPISteps.java
@@ -1,5 +1,6 @@
 package Steps;
 
+import helpers.JsonHelper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,42 +8,71 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class BraveNewCoinAPISteps {
     private static RequestSpecification request;
     private Response response;
     private ValidatableResponse json;
+    private String payloadsFolder = "src/test/resources/Payloads/";
 
     @Given("I have a valid API Key for the {string} URI")
-    public void iSetTheRequestParams(String URI) {
+    public void iSetTheRequestParams(String uri) {
         request = request.given()
                 .header("X-RapidAPI-Key", "")
                 .header("X-RapidAPI-Host", "")
                 .contentType(ContentType.JSON)
-                .baseUri(URI)
+                .baseUri(uri)
                 .log().all();
     }
 
-    @When("I send a POST request with a valid body to the {string} endpoint")
-    public void sendPOSTRequest(String arg0) {
+    @When("I send a POST request with a valid {string} payload to the {string} endpoint")
+    public void sendPOSTRequest(String payload, String endpoint) {
+        File requestBody = new File( payloadsFolder + payload +".json");
+        response = request.when()
+                .body(requestBody)
+                .post(endpoint)
+                .prettyPeek();
     }
 
 
     @Then("I can validate I receive a valid token in the response")
-    public void iCanValidateIReceiveAValidTokenInTheResponse() {
+    public void iGetAValidToken() throws IOException, ParseException {
+        String myToken = JsonHelper.getProperty(
+                new File(payloadsFolder+"MyValidToken.json"),
+                "access_token");
+
+        response.then()
+                .assertThat().statusCode(200)
+                .body("access_token", equalTo(myToken));
     }
 
 
     @Given("I have an invalid API key for the {string} URI")
-    public void iHaveAnInvalidAPIKeyForTheURI(String arg0) {
-        
+    public void iHaveAnInvalidAPIKeyForTheURI(String uri) {
+        request = request.given()
+                .header("X-RapidAPI-Key", "")
+                .header("X-RapidAPI-Host", "")
+                .contentType(ContentType.JSON)
+                .baseUri(uri)
+                .log().all();
     }
 
     @Then("I receive an HTTP Code Status {string}")
-    public void iReceiveAnHTTPCodeStatus(String arg0) {
+    public void iReceiveAnHTTPCodeStatus(String statusCode) {
+        response.then()
+                .assertThat()
+                .statusCode(Integer.parseInt(statusCode));
     }
 
-    @When("I send a POST request without {string} in it's body")
-    public void iSendAPOSTRequestWithoutInItSBody(String arg0) {
+    @When("I send a POST request without {string} in it's body to the {string} endpoint")
+    public void iSendAPOSTRequestWithoutInItSBodyToTheEndpoint(String arg0, String endpoint) {
+
     }
 }
--- src/test/resources/features/BraveNewCoin.feature
+++ src/test/resources/features/BraveNewCoin.feature
@@ -3,16 +3,16 @@ Feature: BraveNewCoin API scenarios.
   Rule: When I send a POST request to the endpoint, I receive a token I can use to make further authentication calls.
 
     Scenario: As a user, I can retrieve a token when making a valid POST request
-      Given I have a valid API Key for the "<uri>" URI
-      When I send a POST request with a valid body to the "endpoint" endpoint
+      Given I have a valid API Key for the "https://bravenewcoin.p.rapidapi.com" URI
+      When I send a POST request with a valid "TokenRequestBody" payload to the "/oauth/token" endpoint
       Then I can validate I receive a valid token in the response
 
     Scenario: As a user, when I use an invalid API key, I get an HTTP Code Status 403
       Given I have an invalid API key for the "<uri>" URI
-      When I send a POST request with a valid body to the "endpoint" endpoint
-      Then I receive an HTTP Code Status "codeStatus"
+      When I send a POST request with a valid "TokenRequestBody" payload to the "/oauth/token" endpoint
+      Then I receive an HTTP Code Status "403"
 
     Scenario: As a user, when I send the POST request without "gran_type", I get an HTTP Code Status 400
-      Given I have a valid API Key for the "<uri>" URI
-      When I send a POST request without "grant_type" in it's body
-      Then I receive an HTTP Code Status "codeStatus"
+      Given I have a valid API Key for the "https://bravenewcoin.p.rapidapi.com" URI
+      When I send a POST request without "grant_type" in it's body to the "/oauth/token" endpoint
+      Then I receive an HTTP Code Status "400"
